715e6263b6261be01aade1c252141e944dd1a5aa
Yannick CHAVANIS <yannick.chavanis.pro@gmail.com>
2026-02-05 11:54:34 +0100

    Add Spark container to docker

--- airflow/dags/sncf_dag.py
+++ airflow/dags/sncf_dag.py
@@ -2,7 +2,6 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
-
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
@@ -10,17 +9,15 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
-
 with DAG(
     dag_id="sncf_to_mongo_datalake",
     default_args=default_args,
     description="Ingestion API SNCF vers MongoDB",
-    schedule_interval="*/5 * * * *",  # Tous les jours 5 minutes
-    start_date=datetime(2025, 1, 1),
+    schedule_interval="*/5 * * * *",  # Toutes les 5 minutes
+    start_date=datetime(2026, 1, 1),
     catchup=False,
     tags=["sncf", "datalake", "mongo"],
 ) as dag:
-
     ingest_task = BashOperator(
         task_id="fetch_sncf_data",
         bash_command="python /opt/airflow/src/extract.py"
