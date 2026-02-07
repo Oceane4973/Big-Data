0d7cfab1ada320331a74f627b473bbb785fa9289
Yannick CHAVANIS <yannick.chavanis.pro@gmail.com>
2026-02-07 15:23:56 +0100

    Create dashboards in Grafana

--- scripts/postgres_organized/setup_grafana.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER "grafana_user" WITH PASSWORD 'grafana_password_123';
-
-GRANT CONNECT ON DATABASE sncf_organized_data TO "grafana_user";
-
-\c sncf_organized_data
-
-GRANT USAGE ON SCHEMA public TO "grafana_user";
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO "grafana_user";
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO "grafana_user";
